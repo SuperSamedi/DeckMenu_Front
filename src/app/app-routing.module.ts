e89a04e41c762535ebf5a6978683fc0f59924587
SuperSamedi <contact.max.dolmans@gmail.com>
2022-09-28 00:20:42 +0200

    deck creation feature - early struggles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { DeckDetailsComponent } from './pages/deck-details/deck-details.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { NewDeckComponent } from './pages/new-deck/new-deck.component';
+import { ShowcaseComponent } from './pages/showcase/showcase.component';
 import { YourDecksComponent } from './pages/your-decks/your-decks.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'decks', component: YourDecksComponent },
   { path: 'decks/:id', component: DeckDetailsComponent },
-  { path: 'decks/new', component: NewDeckComponent }
+  { path: 'decks/new', component: NewDeckComponent },
+  { path: 'recipes/new', component: NewDeckComponent },
+  { path: 'username/menu', component: ShowcaseComponent }
 ];
 
 @NgModule({
--- src/app/pages/new-deck/new-deck.component.ts
+++ src/app/pages/new-deck/new-deck.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DeckFormatService } from 'src/app/services/deck-format.service';
+import { Format } from '../models/format';
 
 @Component({
   templateUrl: './new-deck.component.html',
   styleUrls: ['./new-deck.component.css']
 })
 export class NewDeckComponent implements OnInit {
+
+  deckFormats$: Observable<Format[]>;
+
   newDeckForm = new FormGroup({
     deckName: new FormControl("Deck Name", [Validators.required, Validators.minLength(1), Validators.maxLength(26)]),
     deckFormat: new FormControl("")
   })
-  constructor() { }
+
+  constructor(private _formatService: DeckFormatService, private _router: Router) { }
 
   ngOnInit(): void {
+    this.deckFormats$ = this._formatService.getAllFormats();
   }
 
   onSubmit() {
 
   }
+
+
 
 }
