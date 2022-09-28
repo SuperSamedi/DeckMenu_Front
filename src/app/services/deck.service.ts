57f5653ed5d28df8632b176775e4e682792c5ddb
Max Dolmans <contact.max.dolmans@gmail.com>
2022-09-28 13:53:19 +0200

    adjusted a couple of visuals and improved some variable names

--- src/app/services/deck.service.ts
+++ src/app/services/deck.service.ts
@@ -14,19 +14,20 @@ export class DeckService {
 
 
   create(createDeckForm: any): Observable<any> {
-    return this._http.post(environment.api.url + "/decks/new", createDeckForm);
+    return this._http.post(environment.api.deckmenu + "/decks/new", createDeckForm);
   }
 
   getAll(): Observable<Deck[]> {
-    return this._http.get<Deck[]>(environment.api.url + "/decks/all");
+    return this._http.get<Deck[]>(environment.api.deckmenu + "/decks/all");
   }
 
   getDetails(id: number): Observable<Deck> {
-    return this._http.get<Deck>(environment.api.url + `/decks/` + id);
+    return this._http.get<Deck>(environment.api.deckmenu + `/decks/` + id);
   }
 
-  delete(id: number) {
-    this._http.delete(environment.api.url + "/decks/delete" + id);
+  delete(id: number): Observable<Deck> {
+    console.log("attempting to delete.");
+    return this._http.delete<Deck>(environment.api.deckmenu + "/decks/" + id);
   }
 
 }
